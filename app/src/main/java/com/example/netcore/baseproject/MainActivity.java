e704f1860985a524914365e6618a3be7f2eb4f93
sumitbandekar1 <sumit.bandekar007@gmail.com>
2019-02-12 15:08:47 +0530

    second commit

---
+++ app/src/main/java/com/example/netcore/baseproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.netcore.baseproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//    git config --global user.email "you@example.com"
+    //    git config --global user.name "Your Name"
